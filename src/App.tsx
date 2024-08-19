ad54ae860790af1bc1aa876c3337d273c5a7e538
성민 <63378895+dureotkd@users.noreply.github.com>
2024-08-19 17:55:54 +0900

    Props 및 State에 대한 유형주석

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
+import React from "react";
 
-function App() {
+interface MyComponentProps {
+  name: string;
+  age: number;
+}
+
+/**
+ * 항상 컴포넌트 props와 state에 대한 유형 주석을 제공하여 유형 안전성을
+ * 보장하여 코드 가독성을 개선합니다,
+ * 인터페이스나 유형을 사용하여 props나 state객체의 모양을 정의합니다
+ */
+
+// 제네릭문법 Generic
+const MyComponent: React.FC<MyComponentProps> = ({ name, age }) => {
+  return (
+    <div>
+      {name} & {age}
+    </div>
+  );
+};
+
+// 타입주석 Type Annotation
+const MyComponent2 = ({ name, age }: MyComponentProps) => {
+  return (
+    <div>
+      {name} & {age}
+    </div>
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyComponent name="z" age={5} />
+        <MyComponent2 name="성민" age={30} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
